8aca0db42a4106813539a8ad8bf6cf22f6dc5378
vladmihai98 <vlad.mihai98@gmail.com>
2019-10-29 19:48:53 +0000

    Initial commit - player and camera movement ok

---
+++ Assets/JumperController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperController : MonoBehaviour
+{
+    //TODO remove multiple jump; isGrounded?
+
+    [SerializeField] float movementSpeed = 5;
+    [SerializeField] float jumpHeight = 300;
+
+    private Rigidbody rigidbody;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        Move();
+        Jump();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            velocity = Vector3.forward + Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            velocity = Vector3.forward + Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            velocity = Vector3.back + Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            velocity = Vector3.back + Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            velocity = Vector3.forward;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            velocity = Vector3.left;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity = Vector3.back;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            velocity = Vector3.right;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            velocity = Vector3.zero;
+        }
+    }
+
+    void Move()
+    {
+        rigidbody.MovePosition(transform.position + (velocity * movementSpeed * Time.deltaTime));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(0, jumpHeight, 0);
+        }
+    }
+}
---
+++ Assets/PusherController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusherController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5;
+
+    private Rigidbody rigidbody;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity = Vector3.forward + Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity = Vector3.forward + Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity = Vector3.back + Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity = Vector3.back + Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity = Vector3.forward;
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity = Vector3.left;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity = Vector3.back;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            velocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity = Vector3.right;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            velocity = Vector3.zero;
+        }
+    }
+
+    void Move()
+    {
+        rigidbody.MovePosition(transform.position + (velocity * movementSpeed * Time.deltaTime));
+    }
+}
