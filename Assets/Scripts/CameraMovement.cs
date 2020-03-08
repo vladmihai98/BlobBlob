b4dc50156ae105f74dbbe2f4f2648fd675f114bf
vladmihai98 <vlad.mihai98@gmail.com>
2020-03-08 13:11:52 +0000

    Adjusted the camera script to follow players and hopefully no jittering

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform monty;
+    [SerializeField] Transform seeSharp;
+    [SerializeField] Vector3 positionOffset;
+
+    private Camera camera;
+    private Bounds bounds;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        bounds = GetBounds();
+        PositionCamera();
+    }
+
+    Bounds GetBounds()
+    {
+        Bounds bounds = new Bounds(monty.position, Vector3.zero);
+        bounds.Encapsulate(seeSharp.position);
+        return bounds;
+    }
+
+    void PositionCamera()
+    {
+        Vector3 newPosition = bounds.center + positionOffset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.5f);
+    }
+}
