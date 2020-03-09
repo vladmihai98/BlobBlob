939b815d7f0221f85d47bf5cac0f9dd718a173ec
vladmihai98 <vlad.mihai98@gmail.com>
2020-03-09 18:37:00 +0000

    Added collider for the EnergySlash and cleaned up script for missiles.

---
+++ Assets/Scripts/ParticleSystemController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ParticleSystemController : MonoBehaviour
+{
+    private Ability ability;
+
+    void Start()
+    {
+        ability = GetComponent<Ability>();
+        Destroy(gameObject, ability.Duration);
+    }
+
+    void Update()
+    {
+        if (ability.Speed >= 0)
+        {
+            transform.position += transform.forward * (ability.Speed * Time.deltaTime);
+        }
+    }
+}
